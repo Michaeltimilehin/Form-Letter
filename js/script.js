2f289e040c22548bf8af1dbd328d580c23ccd941
Michaeltimilehin <adurogboyemichael9@gmail.com>
2024-05-23 11:09:32 +0100

    add form letter

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ $(document).ready(function() {
     const recipientnameInput = $("input#recipientname").val();
     const purposeInput= $("input#purpose").val();
     const schoolnameInput =$("input#schoolname").val();
+    const phonenumberInput =$("input#phonenumber").val();
+    
+
 
     $(".yourname").append(yournameInput);
     $(".recipientname").append(recipientnameInput);
     $(".purpose").append(purposeInput);
     $(".schoolname").append(schoolnameInput);
+    $(".phonenumber").append(phonenumberInput);
 
 
     $("#story").show();
