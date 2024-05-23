effad6d22ce88aad51d12b57b9958060d0504889
Michaeltimilehin <adurogboyemichael9@gmail.com>
2024-05-23 12:56:38 +0100

    add letter buttons

--- js/script.js
+++ js/script.js
@@ -2,18 +2,16 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     const yournameInput = $("input#yourname").val();
     const recipientnameInput = $("input#recipientname").val();
-    const purposeInput= $("input#purpose").val();
     const schoolnameInput =$("input#schoolname").val();
     const phonenumberInput =$("input#phonenumber").val();
-    
+    const emailaddressInput =$("input#emailaddress").val();
 
 
     $(".yourname").append(yournameInput);
     $(".recipientname").append(recipientnameInput);
-    $(".purpose").append(purposeInput);
     $(".schoolname").append(schoolnameInput);
     $(".phonenumber").append(phonenumberInput);
-
+    $(".emailaddress").append(emailaddressInput);
 
     $("#story").show();
 
